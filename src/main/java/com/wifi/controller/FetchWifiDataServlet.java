3af6dd363c496e0d83c78e428ae48621d9ce716b
Kingseungil <flykim511@naver.com>
2023-09-20 00:58:37 +0900

    단순 코드 수정

--- src/main/java/com/wifi/controller/FetchWifiDataServlet.java
+++ src/main/java/com/wifi/controller/FetchWifiDataServlet.java
@@ -1,18 +1,25 @@
 package com.wifi.controller;
 
+import com.wifi.service.WifiService;
 import java.io.IOException;
+import java.sql.SQLException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "fetchDataServlet", value = "/load-wifi")
-public class FetchDataServlet extends HttpServlet {
+@WebServlet(name = "fetchWifiDataServlet", value = "/load-wifi")
+public class FetchWifiDataServlet extends HttpServlet {
+
+    public static int fetchData() throws IOException, SQLException {
+        WifiService wifiService = new WifiService();
+        return wifiService.fetchData();
+    }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try {
-            int dataCount = OpenAPI.fetchData();
+            int dataCount = fetchData();
             request.setAttribute("message", dataCount + "개의 WIFI 정보를 정상적으로 저장하였습니다.");
         } catch (Exception e) {
             e.printStackTrace();
